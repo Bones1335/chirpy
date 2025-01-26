9682c0729435f66f07ba37b5af064b26b16d6e93
Alexander Hansen <ahansen1335@gmail.com>
2025-01-26 08:23:11 +0100

    change metrics from api to admin with html instead of plain text

--- main.go
+++ main.go
@@ -35,16 +35,16 @@ func main() {
 		w.Write([]byte(body))
 	})
 
-	mx.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
-	mx.HandleFunc("POST /api/reset", apiCfg.handlerReset)
+	mx.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mx.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	log.Fatal(server.ListenAndServe())
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
